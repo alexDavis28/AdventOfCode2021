e70a7eb2db5a255f9be9448a58aa4bb416b818eb
8m3xl <8m3xlt@gmail.com>
2021-12-03 09:35:02 +0000

    day 3 part 2

--- 3.py
+++ 3.py
@@ -4,7 +4,7 @@ with open("3.txt", "r") as file:
 gamma = ""
 epsilon = ""
 
-for i in range(len(lines[0])-1):
+for i in range(len(lines[0]) - 1):
     count_zero = 0
     count_one = 0
     for line in lines:
@@ -20,4 +20,49 @@ for i in range(len(lines[0])-1):
         epsilon += "1"
 
 output = int(gamma, 2) * int(epsilon, 2)
-print(output)
+# print(output)
+
+# Find oxygen rating
+
+oxygen_lines = lines.copy()
+n = 0
+while len(oxygen_lines) > 1:
+    count_zero = 0
+    count_one = 0
+    for line in oxygen_lines:
+        if line[n] == "0":
+            count_zero += 1
+        elif line[n] == "1":
+            count_one += 1
+    if count_one > count_zero:
+        oxygen_lines = [i for i in oxygen_lines if i[n] == "1"]
+    elif count_one < count_zero:
+        oxygen_lines = [i for i in oxygen_lines if i[n] == "0"]
+    elif count_one == count_zero:
+        oxygen_lines = [i for i in oxygen_lines if i[n] == "1"]
+    n += 1
+
+oxygen = int(oxygen_lines[0], 2)
+
+# Find scrubber
+
+scrubber_lines = lines.copy()
+n = 0
+while len(scrubber_lines) > 1:
+    count_zero = 0
+    count_one = 0
+    for line in scrubber_lines:
+        if line[n] == "0":
+            count_zero += 1
+        elif line[n] == "1":
+            count_one += 1
+    if count_one < count_zero:
+        scrubber_lines = [i for i in scrubber_lines if i[n] == "1"]
+    elif count_one > count_zero:
+        scrubber_lines = [i for i in scrubber_lines if i[n] == "0"]
+    elif count_one == count_zero:
+        scrubber_lines = [i for i in scrubber_lines if i[n] == "0"]
+    n += 1
+
+scrubber = int(scrubber_lines[0], 2)
+print(oxygen*scrubber)
